db0749b0f70b9bbb9f6d4548f1f73b565686c770
Axel Buob <contact@axelbuob.fr>
2022-04-02 11:10:43 +0200

    [feat] Get products and users Fixes #12

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,30 +6,64 @@ use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Validator;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-#[ApiResource]
+#[ApiResource(
+    collectionOperations: ['get', 'post'],
+    itemOperations: ['get', 'delete'],
+    denormalizationContext: ['groups' => ['write']],
+    normalizationContext: ['groups' => ['read']],
+)]
 class User implements PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
+    #[Groups(['read', 'write'])]
     private $id;
 
     #[ORM\Column(type: 'string', length: 180)]
+    #[Groups(['read', 'write'])]
+    #[Validator\NotBlank(message: 'Le champ email ne doit pas être vide.')]
+    #[Validator\Email(message: 'Le champ email doit être un email valide.')]
     private $email;
 
     #[ORM\Column(type: 'string', length: 255)]
+    #[Groups(['read', 'write'])]
+    #[Validator\NotBlank(message: 'Le mot de passe ne doit pas être vide.')]
+    #[Validator\Length(
+        max: 255,
+        maxMessage: 'Le mot de passe ne doit pas faire plus de {{ limit }} caractères.'
+    )]
     private $password;
 
     #[ORM\Column(type: 'string', length: 100)]
+    #[Groups(['read', 'write'])]
+    #[Validator\NotBlank(message: 'Le prénom ne doit pas être vide.')]
+    #[Validator\Length(
+        min: 2,
+        max: 255,
+        minMessage: 'Le nom doit faire au moins {{ limit }} caractères.',
+        maxMessage: 'Le nom ne doit pas faire plus de {{ limit }} caractères.'
+    )]
     private $firstName;
 
     #[ORM\Column(type: 'string', length: 100)]
+    #[Groups(['read', 'write'])]
+    #[Validator\NotBlank(message: 'Le prénom ne doit pas être vide.')]
+    #[Validator\Length(
+        min: 2,
+        max: 255,
+        minMessage: 'Le prénom doit faire au moins {{ limit }} caractères.',
+        maxMessage: 'Le prénom ne doit pas faire plus de {{ limit }} caractères.'
+    )]
     private $lastName;
 
     #[ORM\ManyToOne(targetEntity: Customer::class, inversedBy: 'users')]
     #[ORM\JoinColumn(nullable: false)]
+    #[Groups(['write'])]
     private $customer;
 
     public function getId(): ?int
